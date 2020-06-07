6e0cddd27aa5b1d68c2d067a4bef019dfbe486e9
angoreno <isaac.pizarroc2@gmail.com>
2020-06-07 00:14:30 -0400

    Ejercicio 32 Terminado

---
+++ modulo.3/clase 8 04-06-2020/Desarrollo/Ejercicio 32/ejercicio32.js
@@ -0,0 +1,16 @@
+
+
+$('#texto').click(function(){
+    alert($('#parrafo').text());
+});
+$('#html').click(function(){
+    alert($('#parrf').html());
+});
+$('#val').click(function(){
+    alert($('#tArea').val());
+});
+
+$('#enlace').click(function(){
+    alert($('#link').text());
+});
+
